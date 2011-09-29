806230aa9163ed95ff2580dda3d2ff594cccce38
Rafael George <george.rafael@gmail.com>
2011-09-29 17:09:22 -0400

    added initial account retrieval code

--- spec/account_spec.rb
+++ spec/account_spec.rb
@@ -1,6 +1,11 @@
 require File.expand_path(File.dirname(__FILE__)) + '/spec_helper'
 
 describe "UnfuddleAPI::Acount" do 
+  before(:each) do 
+    ActiveResource::HttpMock.respond_to do |mock|
+    end
+  end
+
   it "should retrieve account information" do 
     UnfuddleAPI.authenticate('username', 'password', 'account')
     account = UnfuddleAPI::Account.find(:all)
